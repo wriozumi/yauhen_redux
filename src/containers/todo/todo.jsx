92c7325d653ccffa44f4c9b45bfaf1edb6d3e145
wriozumi <tshatmanaliev@gmail.com>
2019-07-15 00:08:24 +0600

    add functionality and setup

---
+++ src/containers/todo/todo.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { addTask } from "../../actions/actionCreator";
+
+import ToDoInput from "../../components/todo-input/todo-input.jsx";
+import ToDoList from "../../components/todo-list/todo-list.jsx";
+import Footer from "../../components/footer/footer.jsx";
+
+import "./todo.css";
+
+class ToDo extends Component {
+  state = {
+    activeFilter: "all",
+    taskText: ""
+  };
+
+  handleInputChange = ({ target: { value } }) =>
+    this.setState({
+      taskText: value
+    });
+
+  addTask = ({ key }) => {
+    const { taskText } = this.state;
+    if (taskText.length > 3 && key === "Enter") {
+      const { addTask } = this.props;
+
+      addTask(new Date().getTime(), taskText, false);
+      this.setState({
+        taskText: ""
+      });
+    }
+  };
+
+  render() {
+    const { activeFilter, taskText } = this.state;
+    const { tasks } = this.props;
+    const isTasksExist = tasks && tasks.length > 0;
+
+    return (
+      <div className="todo-wrapper">
+        <ToDoInput
+          onKeyPress={this.addTask}
+          onChange={this.handleInputChange}
+          value={taskText}
+        />
+        {isTasksExist && <ToDoList tasksList={tasks} />}
+        {isTasksExist && (
+          <Footer amount={tasks.length} activeFilter={activeFilter} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ tasks: state.tasks }),
+  { addTask }
+)(ToDo);
